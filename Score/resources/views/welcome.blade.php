728d76a26699b951eb36db852e63524cb227814c
kusunokami <85819430+kusunokami@users.noreply.github.com>
2021-10-16 22:44:21 +0700

    Maain Page
    Perubahan main page menjadi halaman yang dapat memberikan informasi composer dsbnya

---
+++ Score/resources/views/welcome.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <a href="">Score</a> |
+        <a href="">Authorization</a>
+    </nav>
+    <h1>Score Management</h1>
+    <div class="addScore">
+        <button type="button">Add New Score</button>
+    </div>
+    <div>
+        <table class="table"  style="border: 1px solid black" style="border-collapse: collapse">
+            <thead >
+              <tr>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>A</td>
+                <td>B</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>A</td>
+                <td>B</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>@twitter</td>
+                <td>A</td>
+                <td>B</td>
+              </tr>
+            </tbody>
+          </table>
+    </div>
+</body>
+</html>
